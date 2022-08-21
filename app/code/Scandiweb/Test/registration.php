0360e2884881019d703597b7d2f2af0e4aa6d3b2
MKom-Sama <mkomsany3@yahoo.com>
2022-08-22 01:19:38 +0200

    Created new product,

---
+++ app/code/Scandiweb/Test/registration.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @author    Mohammed Komsany <mohammed.komsany@scandiweb.com>
+ * @license   http://opensource.org/licenses/OSL-3.0 The Open Software License 3.0 (OSL-3.0)
+ * @copyright Copyright (c) 2022 Scandiweb, Inc (https://scandiweb.com)
+ */
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Scandiweb_Test',
+    __DIR__
+);
